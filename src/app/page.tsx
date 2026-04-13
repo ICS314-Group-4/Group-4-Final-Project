838f37de5e89ab56ae1433368de589ff65caa8cd
Skyler Remata <skylanderj808@gmail.com>
2026-04-13 09:32:14 -1000

    Create landing page framework
    Removed sample.env, added .env, updated page, layout, footer, and navbar.tsx, updated global.css, updated cheme.prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,29 @@
 import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Mailbox2Flag, People, PeopleFill } from 'react-bootstrap-icons';
 
 /** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" fluid className="py-3">
+    <Container id="landing-page" fluid className="py-5">
+      <Container>
       <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
-        </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
+        <Col lg={6}>
+          <h1>Save time and help more, get started with UH ITS Email Helper</h1>
+          <p>Create and share your own templates or browse and save your favorites. Save time with templates created by fellow ITS student workers</p>
+          <div className='d-flex gap-3 mb-4 flex-wrap'>
+            <button>
+              Explore templates
+            </button>
+            <button>
+              Create a template
+            </button>
+          </div>
         </Col>
       </Row>
+      </Container>
     </Container>
   </main>
 );
+
 
 export default Home;
