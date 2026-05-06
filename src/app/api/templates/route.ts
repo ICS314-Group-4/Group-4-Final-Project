d9f9fa72e384893a751e9da6da0473f168ccd4e6
cko-2 <cko2@hawaii.edu>
2026-05-05 15:35:17 -1000

    changed usage counter

--- src/app/api/templates/route.ts
+++ src/app/api/templates/route.ts
@@ -15,35 +15,16 @@ export async function POST(req: Request) {
     const tId = Number(templateId);
 
     const result = await prisma.$transaction(async (tx) => {
-      const existingUsage = await tx.templateUsage.findUnique({
-        where: {
-          userId_templateId: {
-            userId: userId,
-            templateId: tId,
-          },
-        },
+      await tx.templateUsage.upsert({
+        where: { userId_templateId: { userId, templateId: tId } },
+        update: { updatedAt: new Date() },
+        create: { userId, templateId: tId },
       });
 
-      if (existingUsage) {
-        await tx.templateUsage.update({
-          where: { id: existingUsage.id },
-          data: { updatedAt: new Date() },
-        });
-
-        return await tx.template.findUnique({ where: { id: tId } });
-      } else {
-        await tx.templateUsage.create({
-          data: {
-            userId: userId,
-            templateId: tId,
-          },
-        });
-
-        return await tx.template.update({
-          where: { id: tId },
-          data: { used: { increment: 1 } },
-        });
-      }
+      return await tx.template.update({
+        where: { id: tId },
+        data: { used: { increment: 1 } },
+      });
     });
 
     return NextResponse.json({ 
